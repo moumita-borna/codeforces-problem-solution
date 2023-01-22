9eeb051283e89a6c005dacd82057cc86e17cdc00
Moumita Yeasmin <123243341+moumita-borna@users.noreply.github.com>
2023-01-22 10:28:41 +0600

    solution of codeforces 4A(watermelon)

---
+++ A/CF-4A.c
@@ -0,0 +1,21 @@
+//Bismillahir Rahmanir Rahim
+//https://codeforces.com/problemset/problem/4/A
+
+#include<stdio.h>
+int main()
+{
+    int w;
+
+    scanf("%d",&w);
+    if(w==2)
+    {
+        printf("NO");
+    }
+    else if(w%2==0)
+    {
+        printf("YES");
+    }
+
+    else {printf("NO");}
+    return 0;
+}
